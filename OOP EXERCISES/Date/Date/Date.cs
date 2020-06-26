834b16bfa1002ab15bc74bd32adf4eebd82dee60
Arafa42 <55528241+Arafa42@users.noreply.github.com>
2020-06-26 03:50:21 +0200

    Date oef added

---
+++ OOP EXERCISES/Date/Date/Date.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Date
+{
+   public class Date
+    {
+
+        private int year;
+        public int Year
+        {
+            get { return year; }
+            set { if (value > 0 && value < 3000) { year = value; }
+                  else { Console.WriteLine("YEAR OUT OF RANGE, PLEASE GIVE YEAR BETWEEN 0 AND 3000."); }
+            }
+        }
+
+
+        private int month;
+        public int Month
+        {
+            get { return month; }
+            set { if (value < 12 && value > 0) { month = value; }
+                else { Console.WriteLine("INVALID MONTH..."); }
+            }
+        }
+
+
+        private int day;
+        public int Day
+        {
+            get { return day; }
+            set { if (value < 31 && value > 0) { day = value; }
+                else { Console.WriteLine("INVALID DAY..."); }
+            }
+        }
+
+
+
+
+        public Date(int day, int month, int year)
+        {
+            this.day = day;
+            this.month = month;
+            this.year = year;
+        }
+
+
+        public void DisplayDate()
+        {
+            Console.WriteLine($"{day:D2}/{month:D2}/{year:D4}");
+       
+        }
+
+
+
+    }
+}
---
+++ OOP EXERCISES/Date/Date/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Date
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            Date dt = new Date(01,01,2000);
+            dt.DisplayDate();
+            dt.Month = 100;
+            dt.Year = 200; 
+            dt.DisplayDate();
+        }
+    }
+}
